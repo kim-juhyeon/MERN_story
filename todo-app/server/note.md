f6fb5afe415258e88f940cdef703b8e2a54dd310
jju <totkfka4@gmail.com>
2023-05-19 23:54:04 +0900

    second commit

---
+++ todo-app/server/note.md
@@ -0,0 +1,8 @@
+npm run dev : 서버를 열기
+
+CRUD => Creat Read Update Delete
+
+/ => 읽을 수 있는 모든 작업
+
+데이터베이스는 mongo db , mongoose 패키지를 이용한다.
+dev종속성이 아닐경우 npm i -D @types/mongoose 이용해서 기본 종속성을 설치할 수 있다.
--- todo-app/server/src/index.ts
+++ todo-app/server/src/index.ts
@@ -1,15 +1,36 @@
 //코드가 변경될때마다 재빌드하는 번거러움을 없애기 위해 npm js.com -> ts node dev
 import express from "express";
+import './db';
+
 
 // create a server
 const app = express();
 
-app.get("/",(request,response) => {
-    response.send("<h1>Hello World!!!나는 김주현이다.<h1>");
+app.use(express.json());
+
+//아래 방법을 통해 포스트맨 raw.json을 생성하여 데이터를 가져올 수 있습니다.
+// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//데이터(포스트맨 body)를 불러올 수 있음
+// app.use((req, res, next) => {
+//     req.on("data",(chunk) => {
+//         req.body = JSON.parse(chunk);
+//         next();
+//     });
+//   });
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "I am listening!" });
 });
 
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "I am listening! to create" });
+});
 
 // listen to some port
-app.listen(8000,() => {
-    console.log("listening");
+app.listen(8000, () => {
+  console.log("listening");
 });
