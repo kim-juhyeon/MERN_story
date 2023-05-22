21df67c2aef3f49e04e37f7bc186d4b2b5383bbf
jju <totkfka4@gmail.com>
2023-05-22 14:46:10 +0900

    db

--- todo-app/server/src/db/index.ts
+++ todo-app/server/src/db/index.ts
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
-mongoose.set('strictQuery',true);
+mongoose.set("strictQuery", true);
 mongoose
-//비동기 방식으로 진행되며, 몽고스 DB 연결시 오류가 발생하면 catch함수 발생
   .connect("mongodb://127.0.0.1:27017/note-app")
   .then(() => {
-    console.log("Connected to DB");
+    console.log("DB connected!");
   })
   .catch((err) => {
     console.log(err);
--- todo-app/server/src/index.ts
+++ todo-app/server/src/index.ts
@@ -1,34 +1,25 @@
-//코드가 변경될때마다 재빌드하는 번거러움을 없애기 위해 npm js.com -> ts node dev
 import express from "express";
-import './db';
+import "./db";
 
+import noteRouter from "./routers/note";
 
 // create a server
 const app = express();
 
+// this will parse post request coming from fetch.post() method
 app.use(express.json());
 
-//아래 방법을 통해 포스트맨 raw.json을 생성하여 데이터를 가져올 수 있습니다.
-// app.use(express.json());
+// this will parse post request coming from html form
 app.use(express.urlencoded({ extended: false }));
 
-//데이터(포스트맨 body)를 불러올 수 있음
-// app.use((req, res, next) => {
-//     req.on("data",(chunk) => {
-//         req.body = JSON.parse(chunk);
-//         next();
-//     });
-//   });
-
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.json({ message: "I am listening!" });
-});
-
-app.post("/create", (req, res) => {
-  console.log(req.body);
-  res.json({ message: "I am listening! to create" });
-});
+/**
+ * 
+ * "localhost:8000/note/create"
+"localhost:8000/note"
+"localhost:8000/note/noteId"
+"localhost:8000/note/noteId"
+*/
+app.use("/note", noteRouter);
 
 // listen to some port
 app.listen(8000, () => {
